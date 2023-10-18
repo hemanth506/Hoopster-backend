00c57df711b14044054e90bd176b706bf2887377
G.Hemanth Raaj <hgirirajan@teamhgs.com>
2023-10-18 13:11:39 +0530

    Added player interface

---
+++ src/interfaces/player.interface.ts
@@ -0,0 +1,37 @@
+export interface ITournament {
+  year: number;
+  tournamentId: string;
+  tournamnentName: string;
+}
+
+export interface ITeam {
+  teamId: string;
+  teamName: string;
+}
+
+export interface ITournamentPlayed extends ITournament, ITeam {
+  pointsScored: number;
+}
+
+export interface IPlayerAchievements extends ITournament {
+  achievementId: string;
+  achievementTitle: string;
+}
+
+export interface IPlayer {
+  name: string;
+  phoneNumber: number;
+  tournamentsPlayed: ITournamentPlayed[];
+  email?: string;
+  dateOfBirth?: string;
+  imageURI?: string;
+  favouritePlayer?: string[];
+  achievementsReceived?: IPlayerAchievements[];
+}
+
+export interface IPlayersRepository {
+  createPlayer(playerData: IPlayer): Promise<IPlayer>;
+  fetchPlayerByPhoneNumber(phoneNumber: number): Promise<IPlayer | undefined>;
+  deletePlayerByPhoneNumber(phoneNumber: number): Promise<IPlayer | undefined>;
+  updatePlayerByPhoneNumber(phoneNumber: number, playerData: IPlayer): Promise<IPlayer | undefined>;
+}
